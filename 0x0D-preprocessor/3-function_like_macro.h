47aec69e1cef68b0b212576ee22c52084dda8b12
l3miage-elkhouc <chaymae.elkhou@etu.univ-grenoble-alpes.fr>
2023-03-20 12:59:24 -0700

    3-function_like_macro.h

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,3 +1,6 @@
-#ifndef Function_like_macro
-#define ABS(x) (((x) < 0) ? (-x) : (x))
-#endif
+#ifndef _function_like_macro_h_
+#define _function_like_macro_h_
+
+#define ABS(x) (((x) < (0)) ? ((x) * (-1)) : (x))
+
+#endif /* _function_like_macro_h_ */
